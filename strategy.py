eb0770228ce43399cc0e16cde14b5df933bf5ac7
jmbohan <jmbohan@purdue.edu>
2024-06-04 17:59:50 +0000

    Bet is two everytime seems to work well

--- strategy.py
+++ strategy.py
@@ -42,34 +42,121 @@ def play_roulette(fib_sequence, games):
             if total_loss >= 1000:
                 break
 
-        balance_history.append((i, total_profit - total_loss, total_win, total_loss))
+        balance_history.append((i, total_profit - total_loss, total_win, total_loss, spin_result))
 
     return balance_history
 
 fib_sequence = fibonacci_sequence(100)
-games = 10000
+games = 100
 balance_history = play_roulette(fib_sequence, games)
 
+# Plot the balance history
 x_values = [x[0] for x in balance_history]
 y_values = [x[1] for x in balance_history]
 
-plt.plot(x_values, y_values)
-plt.xlabel('Number of Games')
+plt.plot(x_values, y_values, marker='o')  # Plot the balance history
+plt.xlabel('Number of Hands')
 plt.ylabel('Balance')
-plt.title('Balance over Games')
+plt.title('Balance over 100 Hands')
 
-for x, y, _, _ in balance_history:
+# Highlighting losses with red 'x' markers
+for x, y, _, _, spin_result in balance_history:
     if y < 0:
         plt.scatter(x, y, color='red', marker='x')
+    else:
+        plt.scatter(x, y, color='green', marker='o')
 
-plt.savefig('balance_plot.png')
+plt.savefig('balance_plot.png')  # Save the plot as an image
 plt.show()
 
+# Print the balance history to the console
 print("Balance History:")
-print("Game Number\tTotal Win\tTotal Loss")
+print("Hand Number\tTotal Win\tTotal Loss\tBalance\tWinning Number")
 for item in balance_history:
-    print("{:12}\t{:10}\t{:10}".format(item[0], item[2], item[3]))
+    print("{:12}\t{:10}\t{:10}\t{:10}\t{:14}".format(item[0], item[2], item[3], item[1], item[4]))
 
+# Export the balance history to a text file
 with open('balance_history.txt', 'w') as f:
     for item in balance_history:
-        f.write("{},{},{}\n".format(item[0], item[2], item[3]))
+        f.write("{},{},{},{},{}\n".format(item[0], item[2], item[3], item[1], item[4]))
+import random
+import matplotlib.pyplot as plt
+
+def fibonacci_sequence(n):
+    fib_sequence = [1, 1]  # Start with [1, 1] to avoid zero bets
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    return fib_sequence
+
+def roulette_spin():
+    options = [-1, 0] + list(range(1, 37))
+    weights = [1, 1] + [2] * 36
+    return random.choices(options, weights=weights)[0]
+
+def play_roulette(fib_sequence, games):
+    total_profit = 0
+    total_win = 0
+    total_loss = 0
+    current_bet_index = 0
+    balance_history = []
+    not_hit_count = 0
+
+    for i in range(1, games + 1):
+        current_bet = fib_sequence[current_bet_index]
+        total_bet = current_bet * 2
+        profit = 0
+
+        spin_result = roulette_spin()
+
+        if spin_result in range(1, 25):
+            profit = total_bet * 2
+            total_profit += profit
+            total_win += total_bet
+            current_bet_index = 0
+            not_hit_count = 0
+        else:
+            total_loss += total_bet
+            not_hit_count += 1
+            if not_hit_count >= 15:
+                current_bet_index = (current_bet_index + 1) % len(fib_sequence)
+
+            if total_loss >= 1000:
+                break
+
+        balance_history.append((i, total_profit - total_loss, total_win, total_loss, spin_result, total_bet))
+
+    return balance_history
+
+fib_sequence = fibonacci_sequence(100)
+games = 100
+balance_history = play_roulette(fib_sequence, games)
+
+# Plot the balance history
+x_values = [x[0] for x in balance_history]
+y_values = [x[1] for x in balance_history]
+
+plt.plot(x_values, y_values, marker='o')  # Plot the balance history
+plt.xlabel('Number of Hands')
+plt.ylabel('Balance')
+plt.title('Balance over 100 Hands')
+
+# Highlighting losses with red 'x' markers
+for x, y, _, _, spin_result, total_bet in balance_history:
+    if y < 0:
+        plt.scatter(x, y, color='red', marker='x')
+    else:
+        plt.scatter(x, y, color='green', marker='o')
+
+plt.savefig('balance_plot.png')  # Save the plot as an image
+plt.show()
+
+# Print the balance history to the console
+print("Balance History:")
+print("Hand Number\tTotal Win\tTotal Loss\tBalance\tWinning Number\tTotal Bet")
+for item in balance_history:
+    print("{:12}\t{:10}\t{:10}\t{:10}\t{:14}\t{:9}".format(item[0], item[2], item[3], item[1], item[4], item[5]))
+
+# Export the balance history to a text file
+with open('balance_history.txt', 'w') as f:
+    for item in balance_history:
+        f.write("{},{},{},{},{},{}\n".format(item[0], item[2], item[3], item[1], item[4], item[5]))
